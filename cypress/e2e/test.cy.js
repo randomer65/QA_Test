d2d48433439b33b1eba62fdd552abc4045e9908a
randomer65 <krhalcyonicity@gmail.com>
2025-05-10 00:29:50 +0200

    edit configs

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,3 +3,11 @@ describe('test spec', () => {
     cy.visit('http://127.0.0.1:3000/index.html')
   })
 })
+describe('Click a button', () => {
+  it('should click the button with id "increment-btn"', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('#increment-btn').click();
+  });
+});
+cy.get('#counter').should('have.text', '1');
