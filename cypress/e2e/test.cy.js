cc96675c341da3e18949dc3bef54ae167e3eae76
randomer65 <krhalcyonicity@gmail.com>
2025-05-10 00:55:27 +0200

    add to test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,13 +1,10 @@
 describe('test spec', () => {
-  it('passes', () => {
-    cy.visit('http://127.0.0.1:3000/index.html')
-  })
-})
-describe('Click a button', () => {
-  it('should click the button with id "increment-btn"', () => {
-    cy.visit('http://localhost:3000');
-
+  it('passes happy path', () => {
+    cy.visit('http://localhost:3000/index.html');
+    cy.get('#counter').should('have.text', '0');
     cy.get('#increment-btn').click();
+    cy.get('#counter').should('have.text', '1');
   });
 });
-cy.get('#counter').should('have.text', '1');
+
+  
