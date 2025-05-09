22f37c6d524654ddf9a1cfb461be3d13999aed02
randomer65 <krhalcyonicity@gmail.com>
2025-05-10 01:06:12 +0200

    add test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -6,5 +6,15 @@ describe('test spec', () => {
     cy.get('#counter').should('have.text', '1');
   });
 });
-
+describe('test spec', () => {
+  it('takes a screenshot after increment', () => {
+    cy.visit('http://localhost:3000/index.html');
+    cy.get('#increment-btn').click();
+    cy.get('#counter').should('have.text', '1');
+    // Take a screenshot of the entire page
+    cy.screenshot('counter-after-increment').then(() => {
+      console.log('::notice::Screenshot saved: cypress/screenshots/test.cy.js/counter-after-increment.png');
+});
+  });
+});
   
