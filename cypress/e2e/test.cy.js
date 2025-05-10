f9ebb5ced5e26180e4bc8c43ca4f90d5fea1e33a
randomer65 <krhalcyonicity@gmail.com>
2025-05-10 22:21:32 +0200

    add info

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -6,7 +6,7 @@ describe('test spec 0', () => {
   });
 });
 describe('test spec 1', () => {
-  it('passes happy path', () => {
+  it('passes happy path test', () => {
     cy.visit('http://localhost:3000/index.html');
     cy.get('#counter').should('have.text', '0');
     cy.get('#increment-btn').click();
@@ -18,21 +18,14 @@ describe('test spec 1', () => {
     cy.get('#counter').should('have.text', '4');
     cy.get('#decrement-btn').click();
     cy.get('#counter').should('have.text', '3');
-  });
-});
-describe('test spec 3', () => {
-  it('takes a screenshot after increment', () => {
-    cy.visit('http://localhost:3000/index.html');
-    cy.get('#increment-btn').click();
-    cy.get('#counter').should('have.text', '1');
     // Take a screenshot of the entire page
     cy.screenshot('counter-after-increment').then(() => {
       console.log('::notice::Screenshot saved: cypress/screenshots/test.cy.js/counter-after-increment.png');
     });
   });
 });
-describe('test spec 3', () => {
-  it('clicks a button multiple times and checks counter', () => {
+describe('test spec 2', () => {
+  it('click Increment button multiple times and check counter', () => {
     cy.visit('http://localhost:3000/index.html');
     for (let i = 1; i < 420; i++) {
     // Arbitrary number - trying to keep the test duration under 30 seconds 
@@ -45,8 +38,8 @@ describe('test spec 3', () => {
     });
   });
 });
-describe('test spec 4', () => {
-  it.skip('takes a screenshot after decrement from 0', () => {
+describe('test spec 3', () => {
+  it.skip('decrement from 0', () => {
     // Skipping because it fails by showing -1 not 0
     cy.visit('http://localhost:3000/index.html');
     cy.get('#decrement-btn').click();
@@ -54,6 +47,30 @@ describe('test spec 4', () => {
     // Take a screenshot of the entire page
     cy.screenshot('counter-after-decrement').then(() => {
       console.log('::notice::Screenshot saved: cypress/screenshots/test.cy.js/counter-after-decrement.png');
+    });
+  });
+});
+
+describe('test spec 4', () => {
+  it('click Increment button multiple times and check counter', () => {
+    cy.visit('http://localhost:3000/index.html');
+    for (let i = 1; i < 200; i++) {
+    // Arbitrary number - trying to keep the test duration under 30 seconds 
+      cy.get('#increment-btn').click();
+      cy.get('#counter').should('have.text', i.toString());
+    }
+    // Take a screenshot of the entire page
+    cy.screenshot('counter-after-several-increments').then(() => {
+      console.log('::notice::Screenshot saved: cypress/screenshots/test.cy.js/counter-after-200-increments.png');
+    });
+    for (let i = 198; i > 0; i--) {
+    // Arbitrary number - trying to keep the test duration under 30 seconds 
+      cy.get('#decrement-btn').click();
+      cy.get('#counter').should('have.text', i.toString());
+    }
+    // Take a screenshot of the entire page
+    cy.screenshot('counter-after-several-increments').then(() => {
+      console.log('::notice::Screenshot saved: cypress/screenshots/test.cy.js/counter-after-200-increments-and-decrements.png');
     });
   });
 });
---
+++ test-plan/testplan.md
@@ -0,0 +1,17 @@
+## Overview
+The web page contains an Increment button, a Decrement button and counter initialized to 0.
+The counter should display positive numbers only, it should increment and decrement when pressed accordingly.
+
+## Test Framework
+The e2e testing framework is cypress and the tests can be run on https://github.com/randomer65/QA_Test.
+Select Actions, the most recent passing test, and re-run all jobs.
+To run locally, 
+npx http-server -p 3000 & 
+npx cypress run
+
+## Tests
+- Smoke test: Open the web page, press Increment and verify the counter shows 1.
+- Happy path: Open the web page, Increment several times, decrement, check values.
+- Several increments: Open the web page, press Increment several hundred times and check the value after each increment.
+- Single decrement: Open the web page, decrement once. This test currently fails as the counter shows -1, therefore this test is skipped. 
+- Several increments and decrements: Increment say 200 times, then decrement 200 times and check the counter values.
