29e2cc371056dbff48979b2adf29b87d5158cc8d
randomer65 <krhalcyonicity@gmail.com>
2025-05-10 10:28:55 +0200

    add test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,12 +1,19 @@
-describe('test spec', () => {
+describe('test spec 1', () => {
   it('passes happy path', () => {
     cy.visit('http://localhost:3000/index.html');
     cy.get('#counter').should('have.text', '0');
     cy.get('#increment-btn').click();
     cy.get('#counter').should('have.text', '1');
+    cy.get('#increment-btn').click();
+    cy.get('#counter').should('have.text', '2');
+    cy.get('#increment-btn').click();
+    cy.get('#increment-btn').click();
+    cy.get('#counter').should('have.text', '4');
+    cy.get('#decrement-btn').click();
+    cy.get('#counter').should('have.text', '3');
   });
 });
-describe('test spec', () => {
+describe('test spec 2', () => {
   it('takes a screenshot after increment', () => {
     cy.visit('http://localhost:3000/index.html');
     cy.get('#increment-btn').click();
@@ -14,7 +21,14 @@ describe('test spec', () => {
     // Take a screenshot of the entire page
     cy.screenshot('counter-after-increment').then(() => {
       console.log('::notice::Screenshot saved: cypress/screenshots/test.cy.js/counter-after-increment.png');
-});
+    });
   });
 });
-  
+describe()('test spec 3', () => {
+  it('clicks a button multiple times and checks counter', () => {
+    for (let i = 1; i < 43; i++) {
+      $cy.get('#increment-btn').click();
+      cy.get('#counter').should('have.value', i);
+    }
+  });
+});
